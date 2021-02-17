89d76b3cdb046c5bab2cbbcef9c043c19ddc4e5a
jwillber <1727143740@qq.com>
2021-02-17 20:07:07 +0800

    ShopNc B2B2C 好商城V5 33hao-V5完整源码 新版首页+手机版+新版后台+微信+修复BUG

---
+++ ShopNc/data/cache/adv/9.php
@@ -0,0 +1,12 @@
+<?php defined('In33hao') or exit('Access Invalid!'); return array (
+  'ap_id' => '9',
+  'ap_name' => '首页底部通栏图片广告',
+  'ap_class' => '0',
+  'ap_display' => '2',
+  'is_use' => '1',
+  'ap_width' => '1200',
+  'ap_height' => '90',
+  'adv_num' => '1',
+  'click_num' => '0',
+  'default_content' => '04418235791378401.jpg',
+);
